37b7ec1c17629bbf5dbff213d750110fcbacfa35
buitr <buitr@192.168.1.196>
2024-11-21 07:29:38 -0500

    prueba

---
+++ src/main/java/com/mycompany/prototipo/Prototipo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prototipo;
+
+/**
+ *
+ * @author buitr
+ */
+public class Prototipo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
